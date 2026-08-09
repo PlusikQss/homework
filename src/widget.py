745ef4666cc0f1050f4cb6d6a876a51ffa609e18
Alexandr Korolev <plusikqss@ya.ru>
2026-08-09 17:05:44 +0700

    feat: add filter and masking logic in widget.py

--- src/widget.py
+++ src/widget.py
@@ -1,5 +1,6 @@
 from datetime import datetime
-from .masks import get_mask_card_number, get_mask_account
+
+from .masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(info: str) -> str:
@@ -18,7 +19,8 @@ def mask_account_card(info: str) -> str:
     number = parts[-1]
     prefix = " ".join(parts[:-1])
 
-    # Простая эвристика: если в префиксе есть слово "Счет" (регистронезависимо) — это счёт
+    # Простая эвристика: если в префиксе есть слово "Счет"
+    # (регистронезависимо) — это счёт
     if "счет" in prefix.lower():
         masked_number = get_mask_account(number)
     else:
